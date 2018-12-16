7154b7ccd274e8bad79b3fbb036a3b45fae0c1fa
IdealVillage <958986180@qq.com>
2018-12-16 16:21:36 +0800

    login_token complete

--- src/main/java/com/mall/controller/portal/UserController.java
+++ src/main/java/com/mall/controller/portal/UserController.java
@@ -5,7 +5,9 @@ import com.mall.common.ResponseCode;
 import com.mall.common.ServerResponse;
 import com.mall.pojo.User;
 import com.mall.service.IUserService;
+import com.mall.util.CookieUtil;
 import com.mall.util.JsonUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mall.util.RedisPoolUtil;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -23,45 +27,53 @@ public class UserController {
 
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session,
+                                      HttpServletResponse servletResponse) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             //session只保存本次会话消息,jsessionid保存在的cookie在关闭浏览器后销毁，若要记住用户
             //需要使用token
             //todo 使用jwt实现记住用户
             //session.setAttribute(Const.CURRENT_USER, response.getData());
-            //A3574D95D9579B826FBAA20C952AEA27
+            CookieUtil.writeLoginToken(servletResponse, session.getId());
             RedisPoolUtil.setEx(session.getId(),
                     JsonUtil.stringify(response.getData()),
                     Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
     }
+
     @RequestMapping(value = "logout.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> logout(HttpSession session){
-        //清除session
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout(HttpServletRequest request,HttpServletResponse response) {
+        String loginToken = CookieUtil.readLoginToken(request);
+        CookieUtil.removeLoginToken(request, response);
+        RedisPoolUtil.del(loginToken);
         return ServerResponse.createBySuccess();
     }
 
     @RequestMapping(value = "register.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> register(User user){
+    public ServerResponse<String> register(User user) {
         return iUserService.register(user);
     }
 
     @RequestMapping(value = "check_valid.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> checkValid(String val,String type){
-        return iUserService.checkValid(val,type);
+    public ServerResponse<String> checkValid(String val, String type) {
+        return iUserService.checkValid(val, type);
     }
 
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session){
-        User u = (User) session.getAttribute(Const.CURRENT_USER);
-        if (u==null) {
+    public ServerResponse<User> getUserInfo(HttpServletRequest request) {
+        String loginToken = CookieUtil.readLoginToken(request);
+        if (StringUtils.isEmpty(loginToken)){
+            return ServerResponse.createByErrorMessage("用户未登录,无法获取用户信息");
+        }
+        String userJson = RedisPoolUtil.get(loginToken);
+        User u = JsonUtil.parse(userJson, User.class);
+        if (u == null) {
             return ServerResponse.createByErrorMessage("用户未登录,无法获取用户信息");
         }
         return ServerResponse.createBySuccess(u);
@@ -69,36 +81,38 @@ public class UserController {
 
     @RequestMapping(value = "forget_get_question.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> forgetGetQuestion(String username){
+    public ServerResponse<String> forgetGetQuestion(String username) {
         return iUserService.selectQuestion(username);
     }
+
     @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> checkAnswer(String username,String question,String answer){
+    public ServerResponse<String> checkAnswer(String username, String question, String answer) {
         return iUserService.checkAnswer(username, question, answer);
     }
+
     //忘记密码，重置密码的情况
     @RequestMapping(value = "forget_reset_password.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken){
+    public ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken) {
         return iUserService.forgetResetPassword(username, passwordNew, forgetToken);
     }
 
     @RequestMapping(value = "reset_password.do", method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew) {
-        User u= (User) session.getAttribute(Const.CURRENT_USER);
-        if (u==null){
+        User u = (User) session.getAttribute(Const.CURRENT_USER);
+        if (u == null) {
             return ServerResponse.createByErrorMessage("用户未登录");
         }
-        return iUserService.resetPassword(passwordOld,passwordNew,u);
+        return iUserService.resetPassword(passwordOld, passwordNew, u);
     }
 
     @RequestMapping(value = "update_information.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> updateInformation(HttpSession session,User user){
+    public ServerResponse<User> updateInformation(HttpSession session, User user) {
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser==null){
+        if (currentUser == null) {
             return ServerResponse.createByErrorMessage("用户未登录");
         }
         //防止横向越权,避免传入别的用户的id,而是使用session中保存的id
@@ -106,7 +120,7 @@ public class UserController {
         //不能修改username,且保证更新session后仍有username属性
         user.setUsername(currentUser.getUsername());
         ServerResponse<User> response = iUserService.updateInformation(currentUser);
-        if (response.isSuccess()){
+        if (response.isSuccess()) {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
@@ -114,9 +128,9 @@ public class UserController {
 
     @RequestMapping(value = "get_information.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getInformation(HttpSession session){
+    public ServerResponse<User> getInformation(HttpSession session) {
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser==null){
+        if (currentUser == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "未登录,status=10,强制登录");
         }
         return iUserService.getInformation(currentUser.getId());
